cce72ec5ab7bad3640c8a21f01f1525293330596
AlinaB <bernardezalina@gmail.com>
2023-11-07 14:46:44 +0100

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
  // Aquí tu código
+
+const addElement = document.getElementById('agregar');
+const removeElement = document.getElementById('eliminar');
+const lista = document.getElementById('lista');
+const elementos = [];
+
+function addToList(text) {
+    elementos.push(text)
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(`ELEMENTO: ${text}`))
+    lista.appendChild(li)
+}
+
+addElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newElement = prompt('Escribe elementos a agregar');
+    addToList(newElement);
+});
+
+
+removeElement.addEventListener('click', (e) => {
+    if (elementos.length > 0) {
+        lista.removeChild(lista.lastElementChild)
+    }
+})
+
+
